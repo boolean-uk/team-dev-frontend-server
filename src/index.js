4e3a0a93a8bbff82fea25e19e901dc07fd552460
Carlo Minciacchi <carlo.minciacchi@leapian.com>
2022-11-22 16:07:26 +0000

    Updated insomnia request collection and fixed eslint quotes in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import app from './app.js'
 
-const port = process.env.PORT || "8080"
+const port = process.env.PORT || '8080'
 
 app.listen(port, () => {
   console.log(`\n Server is running on port ${port}\n`)
